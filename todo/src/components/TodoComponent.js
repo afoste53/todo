4224ad855c07c1d4295c4df42155567f11404438
Gabe <mrginch@gmail.com>
2021-07-13 15:45:56 -0400

    formatting commit

--- todo/src/components/TodoComponent.js
+++ todo/src/components/TodoComponent.js
@@ -1,21 +1,52 @@
 import { ButtonGroup, Button, Card } from "react-bootstrap";
+import { useContext } from "react";
+import { FirebaseContext } from "../firebase";
 
-const TodoComponent = ({ todo, deleteTodo }) => {
+const TodoComponent = ({ todo }) => {
+  const { user, firebase } = useContext(FirebaseContext);
+
+  const deleteTodo = async () => {
+    const todoRef = firebase.db.collection("todos").doc(todo.id);
+    await todoRef
+      .delete()
+      .catch((e) => console.error("Error deleting todo", e));
+  };
+
+  const editTodo = async () => {
+    const todoRef = firebase.db.collection("todos").doc(todo.id);
+  };
+
+  const completeTodo = async () => {
+    const todoRef = firebase.db.collection("todos").doc(todo.id);
+    todoRef.get().then((doc) => {
+      if (doc.exists) {
+        todoRef.update({ priority: "complete" });
+      }
+    });
+  };
+
   return (
-    <Card className="todo m-3">
+    <Card className="todo m-1">
       <Card.Header className="bg-navyish">
-        <Card.Title>{todo.title}</Card.Title>
+        <Card.Title>
+          {todo.title.length > 15
+            ? todo.title.slice(0, 14) + "..."
+            : todo.title}
+        </Card.Title>
       </Card.Header>
       <Card.Body
         className={
-          todo.priority === "high"
+          "todo-small-body" &&
+          (todo.priority === "high"
             ? "high-priority-todo"
             : todo.priority == "normal"
             ? "normal-priority-todo"
-            : "low-priority-todo"
+            : "low-priority-todo")
         }
       >
-        {todo.text}
+        {todo.todoText.length > 35
+          ? todo.todoText.slice(0, 34) + "..."
+          : todo.todoText}
       </Card.Body>
       <Card.Footer>
         <ButtonGroup className="d-flex justify-content-around">
@@ -24,7 +55,12 @@ const TodoComponent = ({ todo, deleteTodo }) => {
               <i className="far fa-trash-alt" />
             </span>
           </Button>
-          <Button variant="outline-success">
+          <Button variant="outline-warning" onClick={editTodo}>
+            <span>
+              <i className="far fa-edit" />
+            </span>
+          </Button>
+          <Button variant="outline-success" onClick={completeTodo}>
             <span>
               <i className="far fa-check-circle" />
             </span>
--- todo/src/components/TodoListComponent.js
+++ todo/src/components/TodoListComponent.js
@@ -1,12 +1,52 @@
 import TodoComponent from "./TodoComponent";
+import { useContext, useEffect, useState } from "react";
+import { FirebaseContext } from "../firebase";
 
-const TodoListComponent = ({ todos, searchTerm, deleteTodo }) => {
+const TodoListComponent = ({ searchTerm, deleteTodo }) => {
+  const { firebase, user } = useContext(FirebaseContext);
+  const [todos, setTodos] = useState(null);
+
+  // fn for fetching todos from db
+  const fetchTodos = () => {
+    firebase.db.collection("todos").onSnapshot((snapshot) => {
+      const todos = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      setTodos(todos);
+    });
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const sortByPriority = (t1, t2) => {
+    if (t1.priority == "normal" && t2.priority == "low") {
+      return -1;
+    } else if (t1.priority == "low" && t2.priority == "normal") {
+      return 1;
+    } else if (t1.priority == "complete") {
+      return 1;
+    } else if (t2.priority == "complete") {
+      return -1;
+    } else if (t1.priority > t2.priority) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
   return (
-    <ul className="d-flex flex-row flex-wrap justify-content-center">
+    <ul className="d-flex  flex-wrap justify-content-center">
       {todos
-        .filter((t) => t.title.toLowerCase().includes(searchTerm.toLowerCase()))
+        ?.filter((t) =>
+          t.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .filter((t) => t.owner.id === user.uid)
+        .filter((t) => t.priority !== "complete")
+        .sort(sortByPriority)
         .map((t, i) => (
-          <TodoComponent key={i} todo={t} deleteTodo={deleteTodo} />
+          <TodoComponent key={i} todo={t} />
         ))}
     </ul>
   );
